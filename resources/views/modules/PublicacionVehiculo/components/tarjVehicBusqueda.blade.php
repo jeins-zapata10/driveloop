a5981382ed68df031afeffe6cb78c489d33af552
edgar <edsala2000@gmail.com>
2026-01-20 20:33:43 -0500

    Add CSS styles for vehicle search card component

--- resources/views/modules/PublicacionVehiculo/components/tarjVehicBusqueda.blade.php
+++ resources/views/modules/PublicacionVehiculo/components/tarjVehicBusqueda.blade.php
@@ -1,3 +1,149 @@
+<style>
+:root {
+    --colorprincipal: #c81843;
+    --colorsecundario: #9a1b39;
+    --negro: #282828;
+}
+
+.body-tarjeta {
+    font-size: 62.5%;
+    margin: 0;
+    padding: 0;
+}
+
+h1 {
+    font-family: "Roboto", sans-serif;
+    font-weight: bold;
+    font-size: 36px;
+}
+
+h2 {
+    font-family: "Roboto", sans-serif;
+    font-weight: 400;
+    font-size: 20px;
+}
+
+h3 {
+    font-family: "Roboto", sans-serif;
+    font-weight: bold;
+    font-size: 18px;
+}
+
+.vehtitulo {
+    text-transform: uppercase;
+    margin-top: 10px;
+}
+
+h4 {
+    font-family: "Roboto", sans-serif;
+    font-size: 13px;
+    font-weight: 400;
+}
+
+.subtitulo {
+    font-weight: lighter;
+    font-size: 20px;
+}
+
+.contenedor {
+    background-image: url(../ICONOS/FONDO.jpg);
+    background-color: #1f1f1f;
+    max-width: 100vw;
+    background-size: cover;
+    background-repeat: no-repeat;
+    height: 100vh;
+    color: white;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+}
+
+.text-princ {
+    margin-left: 300px;
+    margin-bottom: 86px;
+}
+
+.fondo-auto {
+    padding-top: 20px;
+    padding-left: 10px;
+}
+
+.tarjeta {
+    width: 330px;
+    height: 361px;
+    background-color: white;
+    color: var(--negro);
+    border-radius: 10px;
+}
+
+.icon-estrella {
+    width: 21px;
+}
+
+.icon-ubi {
+    width: 13px;
+}
+
+.icon-perso {
+    width: 12px;
+}
+
+.info-iconos {
+    display: flex;
+    gap: 38px;
+}
+
+.icono {
+    display: flex;
+    gap: 7px;
+    margin-top: 6px;
+}
+
+.icon-p {
+    margin-top: 22px;
+    margin-bottom: 8px;
+}
+
+.tarj-content {
+    display: flex;
+    justify-content: center;
+    gap: 23px;
+}
+
+.info {
+    margin: 0 38px;
+}
+
+.buttom-princ {
+    display: flex;
+}
+
+.precio {
+    background-color: var(--negro);
+    font-size: 16;
+    color: white;
+    width: 127px;
+    text-align: center;
+    line-height: 3;
+    font-weight: lighter;
+}
+
+.rentar {
+    background-color: var(--colorprincipal);
+    font-size: 16;
+    color: white;
+    text-transform: uppercase;
+    width: 127px;
+    text-align: center;
+    line-height: 3;
+    font-weight: lighter;
+}
+
+.buttom-princ h3:hover {
+    cursor: pointer;
+}
+</style>
+
 <!-- @if(count($vehiculos) > 0)
     @foreach($vehiculos as $vehiculo)
         <div class="tarjeta">
