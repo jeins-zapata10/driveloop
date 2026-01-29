807e051f62c9aaa8367671125df87b00df922b07
Juan Velez <aztvelez@gmail.com>
2026-01-28 20:23:31 -0500

    cambios en app.blade y carrusel agregado

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,11 +8,17 @@
 
     <title>{{ config('app.name', 'Driveloop') }}</title>
 
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@12/swiper-bundle.min.css">
+
     <!-- Icon -->
     <link rel="icon" type="image/svg+xml" href="{{ asset('favicon.svg') }}">
-
+    
+    
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/css/publicacion/pubVeh.css', 'resources/js/app.js'])
+
+
 </head>
 
 <body class="antialiased">
@@ -153,7 +159,7 @@
     </script>
     {{-- Fin Selector dinámico de ciudades según departamento --}}
 
-
+    <script src="https://cdn.jsdelivr.net/npm/swiper@12/swiper-bundle.min.js"></script>
 
 </body>
 
--- resources/views/modules/PublicacionVehiculo/components/tarjVehiculosPrinc.blade.php
+++ resources/views/modules/PublicacionVehiculo/components/tarjVehiculosPrinc.blade.php
@@ -1,4 +1,230 @@
- <div class="body-tarjeta">
+ <div class="body">
+    
+    <div class="contenedor">
+
+        <div class="text-princ">
+            <h1>Autos destacados</h1>
+            <h3 class="subtitulo">Alquila fácil, rápido y seguro, elige el que más te guste.</h3>
+        </div>
+            <div class="container swiper">
+                <div class="card-wrapper">
+                    
+                    <ul class="card-list swiper-wrapper">
+
+                        <li class="card-item swiper-slide">
+
+                            <a href="#" class="card-link"></a>
+                            <img class="fondo-auto" src="AUTO.jpg" alt="Foto auto rojo">
+                                    <div class="info-tarjeta">
+                                        <div class="info">
+
+                                            <h2 class="vehtitulo">Chevrolet spark GT</h2>
+                                            
+                                            <div class="info-iconos">
+                                                <div class="icono">
+                                                    <img class="icon-estrella" src="ICONOS-17.png" alt="icono estrella">
+                                                    <h2>5.0</h2>
+                                                </div>
+                                                <div class="icono">
+                                                    <img class="icon-ubi" src="ICONOS-16.png" alt="icono ubicacion">
+                                                    <h2>Cali</h2>
+                                                </div>
+                                            </div>
+
+                                            <div class="icono icon-p">
+                                                    <img class="icon-perso" src="ICONOS-18.png" alt="icono persona">
+                                                    <h4>5 Personas</h4>
+                                            </div>
+
+                                            <div class="buttom-princ">
+                                                <h3 class="precio">$150.000 / día</h3>
+                                                <h3 class="rentar">Rentar</h3>
+                                            </div>
+
+                                        </div>
+                                    </div>
+                        </li>
+
+                        <li class="card-item swiper-slide">
+
+                            <a href="#" class="card-link"></a>
+                            <img class="fondo-auto" src="AUTO.jpg" alt="Foto auto rojo">
+                                    <div class="info-tarjeta">
+                                        <div class="info">
+
+                                            <h2 class="vehtitulo">Chevrolet spark GT</h2>
+                                            
+                                            <div class="info-iconos">
+                                                <div class="icono">
+                                                    <img class="icon-estrella" src="ICONOS-17.png" alt="icono estrella">
+                                                    <h2>5.0</h2>
+                                                </div>
+                                                <div class="icono">
+                                                    <img class="icon-ubi" src="ICONOS-16.png" alt="icono ubicacion">
+                                                    <h2>Cali</h2>
+                                                </div>
+                                            </div>
+
+                                            <div class="icono icon-p">
+                                                    <img class="icon-perso" src="ICONOS-18.png" alt="icono persona">
+                                                    <h4>5 Personas</h4>
+                                            </div>
+
+                                            <div class="buttom-princ">
+                                                <h3 class="precio">$150.000 / día</h3>
+                                                <h3 class="rentar">Rentar</h3>
+                                            </div>
+
+                                        </div>
+                                    </div>
+                        </li>
+
+                        <li class="card-item swiper-slide">
+
+                            <a href="#" class="card-link"></a>
+                            <img class="fondo-auto" src="AUTO.jpg" alt="Foto auto rojo">
+                                    <div class="info-tarjeta">
+                                        <div class="info">
+
+                                            <h2 class="vehtitulo">Chevrolet spark GT</h2>
+                                            
+                                            <div class="info-iconos">
+                                                <div class="icono">
+                                                    <img class="icon-estrella" src="ICONOS-17.png" alt="icono estrella">
+                                                    <h2>5.0</h2>
+                                                </div>
+                                                <div class="icono">
+                                                    <img class="icon-ubi" src="ICONOS-16.png" alt="icono ubicacion">
+                                                    <h2>Cali</h2>
+                                                </div>
+                                            </div>
+
+                                            <div class="icono icon-p">
+                                                    <img class="icon-perso" src="ICONOS-18.png" alt="icono persona">
+                                                    <h4>5 Personas</h4>
+                                            </div>
+
+                                            <div class="buttom-princ">
+                                                <h3 class="precio">$150.000 / día</h3>
+                                                <h3 class="rentar">Rentar</h3>
+                                            </div>
+
+                                        </div>
+                                    </div>
+                        </li>
+
+                        <li class="card-item swiper-slide">
+
+                            <a href="#" class="card-link"></a>
+                            <img class="fondo-auto" src="AUTO.jpg" alt="Foto auto rojo">
+                                    <div class="info-tarjeta">
+                                        <div class="info">
+
+                                            <h2 class="vehtitulo">Chevrolet spark GT</h2>
+                                            
+                                            <div class="info-iconos">
+                                                <div class="icono">
+                                                    <img class="icon-estrella" src="ICONOS-17.png" alt="icono estrella">
+                                                    <h2>5.0</h2>
+                                                </div>
+                                                <div class="icono">
+                                                    <img class="icon-ubi" src="ICONOS-16.png" alt="icono ubicacion">
+                                                    <h2>Cali</h2>
+                                                </div>
+                                            </div>
+
+                                            <div class="icono icon-p">
+                                                    <img class="icon-perso" src="ICONOS-18.png" alt="icono persona">
+                                                    <h4>5 Personas</h4>
+                                            </div>
+
+                                            <div class="buttom-princ">
+                                                <h3 class="precio">$150.000 / día</h3>
+                                                <h3 class="rentar">Rentar</h3>
+                                            </div>
+
+                                        </div>
+                                    </div>
+                        </li>
+
+                        <li class="card-item swiper-slide">
+
+                            <a href="#" class="card-link"></a>
+                            <img class="fondo-auto" src="AUTO.jpg" alt="Foto auto rojo">
+                                    <div class="info-tarjeta">
+                                        <div class="info">
+
+                                            <h2 class="vehtitulo">Chevrolet spark GT</h2>
+                                            
+                                            <div class="info-iconos">
+                                                <div class="icono">
+                                                    <img class="icon-estrella" src="ICONOS-17.png" alt="icono estrella">
+                                                    <h2>5.0</h2>
+                                                </div>
+                                                <div class="icono">
+                                                    <img class="icon-ubi" src="ICONOS-16.png" alt="icono ubicacion">
+                                                    <h2>Cali</h2>
+                                                </div>
+                                            </div>
+
+                                            <div class="icono icon-p">
+                                                    <img class="icon-perso" src="ICONOS-18.png" alt="icono persona">
+                                                    <h4>5 Personas</h4>
+                                            </div>
+
+                                            <div class="buttom-princ">
+                                                <h3 class="precio">$150.000 / día</h3>
+                                                <h3 class="rentar">Rentar</h3>
+                                            </div>
+
+                                        </div>
+                                    </div>
+                        </li>
+                    </ul>
+
+                    <div class="swiper-pagination"></div>
+
+                    <div class="swiper-button-prev"></div>
+                    <div class="swiper-button-next"></div>
+                </div>
+            </div>
+    </div>
+    </div>
+ 
+    <script>
+        
+        new Swiper('.card-wrapper', {
+            loop: true,
+            spaceBetween: 30,
+
+            // If we need pagination
+            pagination: {
+                el: '.swiper-pagination',
+                clickable: true,
+                dynamicBullets: true,
+            },
+
+            // Navigation arrows
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            },
+
+            breakpoints: {
+                0: {
+                slidesPerView: 1,
+                },
+                768: {
+                slidesPerView: 2,
+                },
+                1024: {
+                slidesPerView: 3,
+                },
+            }
+        });
+
+    </script>
+ <!-- <div class="body-tarjeta">
  
  <div class="contenedor">
 
@@ -117,7 +343,7 @@
                 </div>
             </div>
 
-            <!-- <div class="tarjeta">
+            <div class="tarjeta">
                 <img class="fondo-auto" src="../ICONOS/AUTO.jpg" alt="Foto auto rojo">
                 <div class="info-tarjeta">
                     
@@ -151,8 +377,8 @@
 
 
                 </div>
-            </div> -->
+            </div> 
         </div>
     </div>
 
-</div>
+</div> -->
