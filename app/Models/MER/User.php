78a4c972560990f4422b486da06daaf8af3182fc
yapupiales <yapupiales42@soy.sena.edu.co>
2026-02-03 00:32:53 -0500

    Add codigo reserva a ticket y el pdf de terminos y condiciones (#40)
    * Add codigo de reserva a ticket
    
    * pdf terminos y condiciones
    
    ---------
    
    Co-authored-by: DriveLoop
    Co-authored-by: Jeffer

--- app/Models/MER/User.php
+++ app/Models/MER/User.php
@@ -109,7 +109,7 @@ class User extends Authenticatable implements MustVerifyEmail
 
 	public function reservas()
 	{
-		return $this->hasMany(Reserva::class, 'codusu', 'cod');
+		return $this->hasMany(Reserva::class, 'idusu', 'id');
 	}
 
 	public function tickets()
