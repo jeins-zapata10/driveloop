d4b6dab681264ad1315815e9223be65916f016eb
Jeins Alberto Zapata Muñoz <jeinszapatam@gmail.com>
2026-02-17 11:04:37 -0500

    Correcion al namespace en modelo vehiculo por duplicacion

--- app/Models/MER/Vehiculo.php
+++ app/Models/MER/Vehiculo.php
@@ -3,7 +3,6 @@
 namespace App\Models\MER;
 
 use App\Modules\PublicacionVehiculo\Models\Accesorio;
-use App\Modules\PublicacionVehiculo\Models\Accesorio;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\MER\FotoVehiculo;
