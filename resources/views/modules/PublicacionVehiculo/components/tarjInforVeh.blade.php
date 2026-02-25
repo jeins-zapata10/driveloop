df79052fc8e6d8f170c11cb68cf3d566b5afcb3c
Juan Velez <aztvelez@gmail.com>
2026-02-24 21:39:10 -0500

    creacion del archivo tarjInforVeh, este es el modal de informacion del vehiculo y se modifico el css

---
+++ resources/views/modules/PublicacionVehiculo/components/tarjInforVeh.blade.php
@@ -0,0 +1,110 @@
+<div class="body">
+
+    <div class="container">
+            
+        <div class="retro-inf">
+            <img class="icon-back" src="ICONO-RETROCESO-16.png" alt="">
+            <h2 class="text-veh">Toyota RAV4 Híbrida 2022</h2>
+        </div>
+
+
+        <div class="div-princ">
+                <div class="img-princ">
+                    <div class="princ-car">
+                        <img class="" src="" alt="">
+                    </div>
+                    <div class="img-secund">
+                        <img class="secund-car" src="" alt="">
+                        <img class="secund-car" src="" alt="">
+                        <img class="secund-car" src="" alt="">
+                    </div>
+                </div>
+
+            <div class="div-secund">
+
+                <div class="div-sec-1">
+
+                    <div class="info-veh">
+                        <div class="info-tec">
+                            <h3>Información Técnica</h3>
+                            <p><strong>Tipo:</strong> SUV</p>
+                            <p><strong>Motor:</strong> Híbrido (gasolina + eléctrico)</p>
+                            <p><strong>Tracción:</strong> 4x4</p>
+                            <p><strong>Dirección:</strong> Asistida eléctrica</p>
+                        </div>
+                        
+                        <div class="acces-vehi">
+                            
+                            <h3>Accesorios del Vehículo</h3>
+                            <div class="text-or">
+                                <div class="text-acc">
+                                    <p>Airbag</p>
+                                    <p>Aire acondicionado</p>
+                                    <p>Bluetooth</p>
+                                    <p>Cargador USB</p>
+                                </div>
+                                <div class="text-acc">
+                                    <p>GPS</p>
+                                    <p>Cámara de reversa</p>
+                                    <p>Sensor de parqueo</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+            
+
+                    <div class="tarj-vehi">
+                        <div class="tarj-vehi-inf">
+                            <img src="ICONO-LIKE-16.png" alt="">
+                            <h1 class="text1">Toyota</h1>
+                            <h2 class="text2">RAV4 Híbrida 2022</h2>
+                            <h2 class="text3">$150.000 COP/día</h2>
+                            <h4>Incluye impuestos, seguro y asistencia en carretera.</h4>
+                            
+                            <div class="icon-info">
+                                
+                            </div>
+                            
+                        </div>
+                            <a class="rent-b" href="#">RENTAR</a>
+                    </div>
+
+                </div>   
+
+                <div class="div-inf-2">
+                    <div class="observa">
+                        <h3 class="inf-div-2">Observaciones</h3>
+                        <div class="seccion-border">
+                            <h4 class="config-info margin-bottom">Datos importantes a tener en cuenta:</h4>
+                            <ul class="observaciones config-info">
+                                <li>El vehículo se entrega limpio y con tanque lleno, se debe devolver igual.</li>
+                                <li>No está permitido fumar ni transportar mascotas sin aviso previo.</li>
+                                <li>Sin límite de kilometraje dentro del departamento de Antioquia.</li>
+                                <li>Se recomienda respetar los límites de velocidad establecidos.</li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div class="secc-info2">
+                        <div class="importante">
+                            <h3 class="inf-div-2">Importante</h3>
+                            <div class="seccion-border">
+                                <h4 class="config-info">Verifica las restricciones de pico y placa para los días que necesites el vehículo.</h4>
+                            </div>
+                        </div>
+                        <div class="segur-cober">
+                            <h3 class="inf-div-2">Seguridad y cobertura</h3>
+                            <div class="seccion-border">
+                                <h4 class="config-info">El vehículo cuenta con SOAT vigente, seguro todo riesgo y asistencia 24/7.</h4>
+                                <h4 class="config-info">Incluye servicio de reemplazo en caso de avería durante el alquiler.</h4>
+                            </div>
+                        </div>
+                        
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>
